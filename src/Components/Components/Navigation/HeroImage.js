1e81c986c4a4719b4a597222c14f8badd76548cd
rk8672 <88264158+rk8672@users.noreply.github.com>
2023-06-03 14:52:45 +0530

    Add files via upload

---
+++ src/Components/Components/Navigation/HeroImage.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../../styles/HeroImage.scss";
+
+export default function HeroImage(props) {
+  
+  return (
+    <div className="hero-section">
+      <div className="hero-section-text">
+        <h1>
+          <span>Radha Krishna</span> store  
+        </h1>
+        
+      </div>
+    </div>
+  );
+}
